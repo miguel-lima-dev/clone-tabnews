2274c4fadb41eb2b05136ea6326121a135d59ce7
miguel-lima-dev <mlima.202010@gmail.com>
2025-12-08 15:07:53 +0000

    Adiciona o `lint:check` e o `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Aula do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import App from "next/app";
 
 function Home() {
-    return <h1>Murilo, me empresta 100 reais?? (me manda um audio finalizando o meme pfv)</h1> 
+  return (
+    <h1>
+      Murilo, me empresta 100 reais?? (me manda um audio finalizando o meme pfv)
+    </h1>
+  );
+}
+
+function teste() {
+  return <h1>Jorge</h1>;
 }
 
 export default Home;
-
-
