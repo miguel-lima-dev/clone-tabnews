46a6439ef06c882d85b1b5ab42173f853fb1ccaa
miguel-lima-dev <mlima.202010@gmail.com>
2025-12-12 18:18:42 +0000

    add ´database.js´ and ´.env´ file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-  response.status(200).json("brutal");
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const res = await database.query("SELECT 1+1 as sum;");
+  console.log(res);
+  response.status(200).json({ chave: "brutal" });
 }
 
 export default status;
