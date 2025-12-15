e442c736009db0687b3d7f6410fdb36408afb6a2
miguel-lima-dev <mlima.202010@gmail.com>
2025-12-15 13:28:16 +0000

    move `.env` file to `.env.development`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,6 +1,6 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch(
-    "https://fluffy-garbanzo-97r59j744gv62xr6r-3001.app.github.dev/api/v1/status",
+    "https://fluffy-garbanzo-97r59j744gv62xr6r-3000.app.github.dev/api/v1/status",
   );
   expect(response.status).toBe(200);
 });
